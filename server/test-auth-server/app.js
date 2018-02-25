a72b13641bab23537a6c8900edd120554f9841c1
DESKTOP-5ND3FOT\Mohamed <mohamed.abozyd@gmail.com>
2018-02-25 23:07:40 +1300

    add mqtt

--- server/test-auth-server/app.js
+++ server/test-auth-server/app.js
@@ -7,10 +7,14 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var mqtt = require('./mqtt/test');
+var mqttController = require('./routes/mqtt');
+var mqttServer =   mqtt;
 
+mqttServer.init();
 var app = express();
 
-// view engine setup
+// view engine setup 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/mqtt', mqttController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
